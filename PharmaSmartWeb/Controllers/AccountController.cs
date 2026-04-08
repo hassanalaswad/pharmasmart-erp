fe8645758672df0fe5db3810c9bf3241835b606c
hassanalaswad <hasanalasod35@gmail.com>
2026-04-08 18:31:41 +0300

    Remove unnecessary blank line in AccountController

--- PharmaSmartWeb/Controllers/AccountController.cs
+++ PharmaSmartWeb/Controllers/AccountController.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Linq;
 using System;
-
 namespace PharmaSmartWeb.Controllers
 {
     // 🚀 يرث من BaseController للوصول لمحرك العزل وخدمة RecordLog
